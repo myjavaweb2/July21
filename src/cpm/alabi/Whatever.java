1765cd83ec82c9141a2c8f331c0cb9613833c047
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-07-21 10:26:43 -0400

    initial commit 07/21/2016 at 10:26 am

---
+++ src/cpm/alabi/Whatever.java
@@ -0,0 +1,13 @@
+package cpm.alabi;
+
+public class Whatever {
+
+	public static void main(String[] args) 
+	{
+		String memo = "I am still picking the pieces on float and double";
+		
+		System.out.println(memo);
+
+	}
+
+}
