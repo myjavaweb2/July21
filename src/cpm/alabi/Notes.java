9104054f7ef60ef47dc9d15e566f01c899912dfd
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-07-21 14:23:07 -0400

    Commit July 21

---
+++ src/cpm/alabi/Notes.java
@@ -0,0 +1,16 @@
+package cpm.alabi;
+
+public class Notes {
+
+	public static void main(String[] args) 
+	{
+		// Statement about class
+		
+		for (int i = 0; i < 5; i++) 
+		{
+			System.out.println("Hello class!");
+		}
+	
+	}
+
+}
